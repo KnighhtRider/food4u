6907344add72eb0854db963ac7699fb44d8a6245
Vivek Thakur <110936458+KnighhtRider@users.noreply.github.com>
2024-01-27 02:30:58 +0530

    bootstrap dark changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 } from 'react-router-dom';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
-import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+// import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import { CartProvider } from './components/ContextReducer';
