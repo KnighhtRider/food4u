06d64da028fa4aa210aed83a52e504a11f9d6a8d
Vivek Thakur <110936458+KnighhtRider@users.noreply.github.com>
2024-02-02 04:44:33 +0530

    :fire: Origin Added

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 mongodb();
 
 app.use(cors({
-  origin: [""],
+  origin: ["https://food4u-phi.vercel.app"],
   methods: ["POST", "GET"],
   credentials: true
 }))
